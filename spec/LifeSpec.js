d1a2fb23b1ed47a9e6805d902249c665738e6294
Linda <lin.m.gonzalez@gmail.com>
2012-10-07 18:48:06 -0400

    Can create cell and determine board size
    Board can measure broswer size
    Board can draw a row of cells
    pending: test for drawing a row of cells

--- spec/LifeSpec.js
+++ spec/LifeSpec.js
@@ -1,13 +1,49 @@
-
-
 describe("Board", function() {
-  //divide window size by cell size
+  //draw a row of cells
+	// 52.times do |i|
+	//    draw a cell   
+	//end
   //make a board div 
   //fill board div 
 
-  it("should be able to measure the window size", function() {
-    
-    var w = board.width
-    expect(w).toEqual(window.innerWidth -10);
+  beforeEach(function() {
+    board = new Board();
   });
+
+  it("should be able to measure the window size", function() {
+		var width = board.window_width();
+    var height = board.window_height();
+		expect(width).toEqual(window.innerWidth -10);
+		expect(height).toEqual(window.innerHeight -10);
+  });
+
+	it("should have a cell size", function() {
+		var cell_size = board.cell_size();
+		expect(cell_size).toEqual(24);
+	});
+	
+	it("should be able to calculate the number of cells to draw per row", function() {
+		var cells_per_row = board.cells_per_row();
+		var cells_that_fit_in_row = Math.floor(board.window_width()/board.cell_size() );
+		expect(cells_per_row).toEqual(cells_that_fit_in_row);
+	});
+	
+	it("should be able to calculate the number of rows", function() {
+		var number_of_rows = board.number_of_rows();
+		var rows_that_fit_in_window = Math.floor(board.window_height()/board.cell_size() );
+		expect(number_of_rows).toEqual(rows_that_fit_in_window);
+	});
+	
+	it("should populate a row with cells", function() {
+		var max_cells = board.cells_per_row();
+		board.draw_row();
+		cells = $('.cell-alive');
+		first = cells.first();
+		last = cells.last();
+		expect(first[0].id).toEqual("1-1");
+		//expect(last[0].id).toEqual(max_cells + "-1");
+	});
+	
 });
+
+
--- src/Life.js
+++ src/Life.js
@@ -1,8 +1,48 @@
-  var Board = {
-      width :  window.innerWidth -10,
-      height : window.innerHeight -10,
-      cell_width : 24,
-      cells_across : function() { return Math.floor(this.width/this.cell_width) },
-      cells_high : function() { return Math.floor(this.height/this.cell_width) },
-      get_size : function() { return [this.width, this.height] },
-    };
+  // var Board = {
+  //     width :  window.innerWidth -10,
+  //     height : window.innerHeight -10,
+  //     cell_width : 24,
+  //     cells_across : function() { return Math.floor(this.width/this.cell_width) },
+  //     cells_high : function() { return Math.floor(this.height/this.cell_width) },
+  //     get_size : function() { return [this.width, this.height] },
+  //   };
+
+// alert("hey, javascript is working");
+
+function Board() {
+}
+
+Board.prototype.window_width = function() {
+  return window.innerWidth -10;
+};
+
+Board.prototype.window_height = function() {
+  return window.innerHeight -10;
+};
+
+Board.prototype.cell_size = function() {
+  return 24;
+};
+
+Board.prototype.cells_per_row = function() {
+	return Math.floor( this.window_width()/this.cell_size() );
+};
+
+Board.prototype.number_of_rows = function() {
+	return Math.floor( this.window_height()/this.cell_size());
+};
+
+// Board.prototype.draw_row = function() {
+// 	var max_cells = this.cells_per_row();
+// 	var current_board = $('#game-board');
+// 	for(var i = 0; i<max_cells; i++){
+// 		current_board.append("<div id='" + (i+1) +  "-1' class='cell-alive'></div>");
+// 	}
+// };
+
+//   this.isPlaying = true;
+// };
+// 
+// Board.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
